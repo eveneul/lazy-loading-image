50a0763d9982c09f1bb57ccc4c57aa82ec326715
eveneul <nebulas9724@gmail.com>
2025-12-10 19:29:15 +0900

    feat: 스크롤 인터렉션 추가

--- js/main.js
+++ js/main.js
@@ -6,3 +6,25 @@ window.addEventListener('DOMContentLoaded', () => {
 		new Rivus(rivus)
 	})
 })
+
+const images = document.querySelectorAll('.c-image')
+
+const inViewObserver = new IntersectionObserver(
+	(entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				const image = entry.target.querySelector('.c-image__image')
+				animDepixelate(image)
+			}
+		})
+	},
+	{
+		root: null,
+		rootMargin: '1px',
+		threshold: 1
+	}
+)
+
+images.forEach((image) => {
+	inViewObserver.observe(image)
+})
